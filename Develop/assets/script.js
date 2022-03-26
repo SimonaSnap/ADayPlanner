ee1408f1e6716e1e8ae0e65eb34e8992df5da11d
Simona <simona.snapk@gmail.com>
2022-03-25 20:09:05 -0700

    tasks are being stored and sent back, and also localstorage clears at the end of the day

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -7,15 +7,13 @@ $("#currentDay").text(day);
 
 var currentHour = moment().hour();
 var tableBody = document.getElementById('tbody');
-var saveBtn = document.getElementById("saveBtn");
-var task = document.createElement("input");
 
 
 
 //adding the rows to the table
-//for the morning
-for (var i = 0; i < 12; i++)
+for (var i = 0; i < 24; i++)
 {
+    //creating the individual row and columns of the table
     var tr = document.createElement("tr");
     tableBody.appendChild(tr);
 
@@ -27,58 +25,111 @@ for (var i = 0; i < 12; i++)
     tr.appendChild(section2);
     tr.appendChild(section3);
 
-    section1.textContent = 1 + i + " AM";
-    section2.innerHTML = "<input></input>";
+    //setting up the individual hour sections - or the first column
+    if (i < 11)
+    {
+        section1.textContent = 1 + i + " AM";
+    }
+    else if (i == 11)
+    {
+        section1.textContent = 1 + i + " PM";
+    }
+    else if (i > 11 && i < 23)
+    {
+        section1.textContent = (1 + (i - 12)).toString() + " PM";
+    }
+    else if (i == 23)
+    {
+        section1.textContent = (1 + (i - 12)).toString() + " AM";
+    }
+
+    //setting up the middle section - the second column
+    var writeTask = document.createElement("input");
+    writeTask.type = "text";
+    writeTask.value = "";
+    writeTask.dataset.place = "written";
+    section2.appendChild(writeTask);
+    //section2.innerHTML = "<input></input>";
+
+    if (i < (currentHour - 1))
+    {
+        section2.style.backgroundColor = "whitesmoke";
+    } else if (i == (currentHour - 1))
+    {
+        section2.style.backgroundColor = "lightcoral";
+    } else
+    {
+        section2.style.backgroundColor = "lightgreen";
+    }
+
+    //setting up the third section - the third column
     var buttonImage = document.createElement("img");
     buttonImage.src = './assets/images/noun-save-2269969.png';
-    buttonImage.style = 'width: 25px; height: 25px;';
+    buttonImage.style = 'width: 30px; height: 30px;';
     var saveBtn = document.createElement("button");
     saveBtn.appendChild(buttonImage);
-    //saveBtn.innerHTML = "<img src='./assets/images/noun-save-2269969.png' style= 'width: 25px; height: 25px;'></img>"
     section3.appendChild(saveBtn);
-
+    //saveBtn.innerHTML = "<img src='./assets/images/noun-save-2269969.png' style= 'width: 25px; height: 25px;'></img>"
     //section3.innerHTML = "<button><img src='./assets/images/noun-save-2269969.png' style= 'width: 25px; height: 25px;'></button>"
 
-    if (i < (currentHour - 1))
+
+
+    //making the application save the input
+    tableBody.addEventListener("click", function (event)
     {
-        section2.style.backgroundColor = "whitesmoke";
-    } else if (i == (currentHour - 1))
+        event.stopImmediatePropagation();
+        var submission = event.currentTarget;
+
+        if (submission.matches("saveBtn"))
+        {
+            var task = submission.getAttribute("data-place");
+            console.log(submission.parentElement.task.value);
+        }
+
+        var savedTasks = [];
+        var task = writeTask.value;
+        var inStorage = localStorage.getItem("task");
+        if (null != inStorage)
+        {
+            savedTasks = inStorage.split(",");
+        }
+
+        console.log(writeTask.value);
+
+        if (task === "")
+        {
+        }
+        else
+        {
+            section2.textContent = task;
+            savedTasks.push(task);
+            localStorage.setItem("task", savedTasks.toString());
+        }
+    })
+
+    function tasksWritten()
     {
-        section2.style.backgroundColor = "lightcoral";
-    } else
+        var allTasks = [];
+        var taskStorage = localStorage.getItem("task");
+        if (null != taskStorage)
+        {
+            allTasks = taskStorage.split(",");
+        }
+
+        for (var x = 0; x < allTasks.length; x++)
+        {
+            var savedTask = "";
+            savedTask = allTasks.toString();
+            var taskNode = document.createTextNode(savedTask);
+            section2.appendChild(taskNode);
+        }
+    }
+    tasksWritten();
+
+    if (currentHour == 24)
     {
-        section2.style.backgroundColor = "lightgreen";
+        localStorage.clear();
     }
 
 }
-
-//for the evening
-for (var i = 12; i < 24; i++)
-{
-    var tr = mainTable.insertRow();
-
-    var section1 = document.createElement("td");
-    var section2 = document.createElement("td");
-    var section3 = document.createElement("td");
-
-    tr.appendChild(section1);
-    tr.appendChild(section2);
-    tr.appendChild(section3);
-
-    section1.textContent = (1 + (i - 12)).toString() + " PM"
-    section2.innerHTML = "<input></input>";
-    section3.innerHTML = "<button><img src='./assets/images/noun-save-2269969.png' style= 'width: 25px; height: 25px;'></button>"
-
-    if (i < (currentHour - 1))
-    {
-        section2.style.backgroundColor = "whitesmoke";
-    } else if (i == (currentHour - 1))
-    {
-        section2.style.backgroundColor = "lightcoral";
-    } else
-    {
-        section2.style.backgroundColor = "lightgreen";
-    }
-}
-
 
